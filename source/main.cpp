ff1626c0800385e57e87172abc67762e9524b810
PoloNX <geyrecharlie@gmail.com>
2021-06-04 23:12:21 +0200

    new ui, no image

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
 #include <switch.h>
 #include <string>
 #include <iostream>
@@ -16,6 +17,7 @@ int main()
     nxlinkStdio();  //redirect cout/cer to tcp
     
     SDL_Init(SDL_INIT_EVERYTHING);  //Init for SDL2
+    IMG_Init(IMG_INIT_PNG);
     TTF_Init(); //Init for SDL_ttf
     SDL_InitSubSystem(SDL_INIT_EVERYTHING); //Some init 
     SDL_JoystickEventState(SDL_ENABLE); //Joystick
@@ -24,36 +26,73 @@ int main()
     romfsInit();    //Init of romfs
     chdir("romfs:/");   //go on romfs
 
-    TTF_Font* font = TTF_OpenFont("data/arial.ttf", 28);    //font for text
-    TTF_Font* font_big = TTF_OpenFont("data/arial.ttf", 50);    //font for title
+    TTF_Font* font_small = TTF_OpenFont("data/BerlinSansFB.ttf", 24);
+    TTF_Font* font = TTF_OpenFont("data/BerlinSansFB.ttf", 48);    //font for text
+    TTF_Font* font_big = TTF_OpenFont("data/BerlinSansFB.ttf", 72);    //font for title
     SDL_Window* mainWindow = SDL_CreateWindow("mainWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, largeur, longueur, SDL_WINDOW_RESIZABLE);   //The window
     SDL_Renderer* mainRenderer = SDL_CreateRenderer(mainWindow, -1, 0); //The renderer
 
-    SDL_Surface* sigpatch_fusee_s = TTF_RenderText_Blended(font, "Download sigpatch for fusee-primary.", SDL_Color {255, 255, 255, 255}); //We create the option for fusee_primary
+    SDL_Surface* sigpatch_fusee_s = TTF_RenderText_Blended(font, "Fusee-Primary", SDL_Color {255, 255, 255, 255}); //We create the option for fusee_primary
     SDL_Texture* sigpatch_fusee = SDL_CreateTextureFromSurface (mainRenderer, sigpatch_fusee_s); //We convert the surface to texture
 
-    SDL_Surface* sigpatch_hekate_s = TTF_RenderText_Blended(font, "Download sigpatchs for hekate.", SDL_Color {255, 255, 255, 255}); //We create the option for hekate
+    SDL_Surface* sigpatch_hekate_s = TTF_RenderText_Blended(font, "Hekate", SDL_Color {255, 255, 255, 255}); //We create the option for hekate
     SDL_Texture* sigpatch_hekate = SDL_CreateTextureFromSurface (mainRenderer, sigpatch_hekate_s); //We convert the surface to texture
 
-    SDL_Surface* title_s = TTF_RenderText_Blended(font_big, "Sigpatch-Downloader", SDL_Color {255, 255, 255, 255}); //We create the title
+    SDL_Surface* title_s = TTF_RenderText_Blended(font_big, "SigPatch Downloader", SDL_Color {255, 255, 255, 255}); //We create the title
     SDL_Texture* title = SDL_CreateTextureFromSurface (mainRenderer, title_s); //We convert the surface to texture
+
+    SDL_Surface* description_s = TTF_RenderText_Blended(font, "Download sigpatch on your Switch.", SDL_Color {255, 255, 255, 255}); //We create the option for hekate
+    SDL_Texture* description = SDL_CreateTextureFromSurface (mainRenderer, description_s); //We convert the surface to texture   
+
+    SDL_Surface* credit_s = TTF_RenderText_Blended(font_small, "By PoloNX", SDL_Color {255, 255, 255, 255}); //We create the option for hekate
+    SDL_Texture* credit = SDL_CreateTextureFromSurface (mainRenderer, credit_s); //We convert the surface to texture 
 
     SDL_Surface* download_s = TTF_RenderText_Blended(font, "Download starting...", SDL_Color {255, 0, 0, 255}); //We create the title
     SDL_Texture* download = SDL_CreateTextureFromSurface (mainRenderer, download_s); //We convert the surface to texture
 
-    SDL_Surface* downloadEnd_s = TTF_RenderText_Blended(font_big, "Download finish. please reboot the Nintendo Switch", SDL_Color {0, 0, 255, 255}); //We create the title
+    SDL_Surface* downloadEnd_s = TTF_RenderText_Blended(font, "Download finish. please reboot the Nintendo Switch", SDL_Color {0, 0, 255, 255}); //We create the title
     SDL_Texture* downloadEnd = SDL_CreateTextureFromSurface (mainRenderer, downloadEnd_s); //We convert the surface to texture
 
+    SDL_Surface* amsLogo_s = IMG_Load("data/fusee.png");
+    SDL_Texture* amsLogo = SDL_CreateTextureFromSurface(mainRenderer, amsLogo_s);
+
+    SDL_Surface* hekateLogo_s = IMG_Load("data/hekate.png");
+    SDL_Texture* hekateLogo = SDL_CreateTextureFromSurface(mainRenderer, hekateLogo_s); 
+
+    SDL_Rect rect_cursorAms;
+    rect_cursorAms.w = sigpatch_fusee_s->w + 50;
+    rect_cursorAms.h = sigpatch_fusee_s->h + 20;
+    rect_cursorAms.x = 75;
+    rect_cursorAms.y = longueur - 210;
+
+    SDL_Rect rect_cursorHekate;
+    rect_cursorHekate.w = sigpatch_fusee_s->w + 50;
+    rect_cursorHekate.h = sigpatch_fusee_s->h + 20;
+    rect_cursorHekate.x = 900;
+    rect_cursorHekate.y = longueur - 210;
+
+    SDL_Rect rect_amsLogo;
+    rect_amsLogo.w = amsLogo_s->w;
+    rect_amsLogo.h = amsLogo_s->h;
+    rect_amsLogo.x = 0;
+    rect_amsLogo.y = 800;
+
+    SDL_Rect rect_hekateLogo;
+    rect_hekateLogo.w = hekateLogo_s->w;
+    rect_hekateLogo.h = hekateLogo_s->h;
+    rect_hekateLogo.x = largeur - 500;
+    rect_hekateLogo.y = 800;
+    
     SDL_Rect rect_fusee;	//Rect AMS
     rect_fusee.w = sigpatch_fusee_s->w;
     rect_fusee.h = sigpatch_fusee_s->h;
-    rect_fusee.x = largeur / 2 - rect_fusee.w / 2;
-    rect_fusee.y = longueur - 400;
+    rect_fusee.x = 100;
+    rect_fusee.y = longueur - 200;
 
     SDL_Rect rect_hekate;	//Rect hekate
     rect_hekate.w = sigpatch_hekate_s->w;
     rect_hekate.h = sigpatch_hekate_s->h;
-    rect_hekate.x = largeur / 2 - rect_hekate.w / 2;
+    rect_hekate.x = 1000;
     rect_hekate.y = longueur - 200;
 
     SDL_Rect rect_title;	//Rect title
@@ -61,6 +100,18 @@ int main()
     rect_title.h = title_s->h;
     rect_title.x = largeur / 2 - rect_title.w / 2;
     rect_title.y = 20;
+
+    SDL_Rect rect_credit;	//Rect title
+    rect_credit.w = credit_s->w;
+    rect_credit.h = credit_s->h;
+    rect_credit.x = largeur / 2 - rect_credit.w / 2;
+    rect_credit.y = longueur - 40;   
+
+    SDL_Rect rect_description;	//Rect title
+    rect_description.w = description_s->w;
+    rect_description.h = description_s->h;
+    rect_description.x = largeur / 2 - rect_description.w / 2;
+    rect_description.y = 100;  
 
     SDL_Rect rect_download;	//Rect title
     rect_download.w = download_s->w;
@@ -77,6 +128,8 @@ int main()
     SDL_FreeSurface(sigpatch_fusee_s); //We Delete Surface
     SDL_FreeSurface(sigpatch_hekate_s);
     SDL_FreeSurface(title_s);
+    SDL_FreeSurface(description_s);
+    SDL_FreeSurface(credit_s);
     SDL_FreeSurface(download_s);
     SDL_FreeSurface(downloadEnd_s);
 
@@ -85,6 +138,8 @@ int main()
     int downloadIsFinish = 0;
     SDL_Event event;	//Event
     chdir("sdmc:/");    //Go in the sdmc
+
+    std::cout << "avant while" << std::endl;
 
     while (isOpen && appletMainLoop())
     {
@@ -104,16 +159,24 @@ int main()
         SDL_RenderClear(mainRenderer);  //We clear the screen
 
         while(SDL_PollEvent(&event)){   //Events
+            if (event.type == SDL_QUIT)
+                isOpen = false;
+            
             if (event.type == SDL_JOYBUTTONDOWN)    //Pressed key
             {
-                if (event.jbutton.button == 15)	//down
+                if (event.jbutton.button == 14)	//right
                 {
                     ++choice;
                 }
 
-                if (event.jbutton.button == 13)	//up
+                if (event.jbutton.button == 12)	//left
                 {
                     --choice;
+                }
+
+                if (event.jbutton.button == 10)
+                {
+                    isOpen = false;
                 }
 
                 if (event.jbutton.button == 0 && choice == 0 && downloadIsFinish == 0)   //Button A and cursor on fusee
@@ -146,6 +209,10 @@ int main()
             SDL_RenderCopy(mainRenderer, sigpatch_fusee, NULL, &rect_fusee); //print de text (fusee)
             SDL_RenderCopy(mainRenderer, sigpatch_hekate, NULL, &rect_hekate); //print de text (hekate)
             SDL_RenderCopy(mainRenderer, title, NULL, &rect_title); //print de text (title)
+            SDL_RenderCopy(mainRenderer, description, NULL, &rect_description);
+            SDL_RenderCopy(mainRenderer, credit, NULL, &rect_credit);
+            SDL_RenderCopy(mainRenderer, hekateLogo, NULL, &rect_hekateLogo);
+            SDL_RenderCopy(mainRenderer, amsLogo, NULL, &rect_amsLogo);
         }
         else if (downloadIsFinish == 1){
             SDL_RenderCopy(mainRenderer, downloadEnd, NULL, &rect_downloadEnd);
@@ -154,18 +221,29 @@ int main()
         if (choice == 0 && downloadIsFinish == 0) //Draw cursor on fusee
         {
             SDL_SetRenderDrawColor(mainRenderer, 123, 224, 228, 255);
-            SDL_RenderDrawRect(mainRenderer, &rect_fusee);
+            SDL_RenderDrawRect(mainRenderer, &rect_cursorAms);
         }
 
         if (choice == 1 && downloadIsFinish == 0) //Draw cursor on hekate
         {
             SDL_SetRenderDrawColor(mainRenderer, 123, 224, 228, 255);
-            SDL_RenderDrawRect(mainRenderer, &rect_hekate);
+            SDL_RenderDrawRect(mainRenderer, &rect_cursorHekate);
         }
 
         SDL_RenderPresent(mainRenderer); //Render Present
 
     }
 
+    SDL_DestroyTexture(sigpatch_fusee);
+    SDL_DestroyTexture(sigpatch_hekate);
+    SDL_DestroyTexture(title);
+    SDL_DestroyTexture(description);
+    SDL_DestroyTexture(credit);
+    SDL_DestroyTexture(downloadEnd);
+    SDL_DestroyTexture(download);
+    TTF_Quit();
+    IMG_Quit();
     SDL_Quit(); //Quit
+    romfsExit();
+    return 0;
 }
